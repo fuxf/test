94aff7b129df2d2bd6f819ee7e97bd6c83d0307a
fuxf <384361279@qq.com>
2017-02-09 16:30:20 +0800

    test1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("哔哩哔哩!");
+        System.out.println("哇哈哈!");
+        System.out.println("哇哈哈!");
+        System.out.println("哇哈哈!");
+        System.out.println("哇哈哈!");
+        System.out.println("哇哈哈!");
+    }
+}
